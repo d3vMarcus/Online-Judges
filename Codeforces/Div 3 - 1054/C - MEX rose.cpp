f26934a2a53d5bb188d5fc62091490c975a5fa6d
Marcus Vinicius Franco Ribeiro <ribeiro.marcus.dev@gmail.com>
2025-10-27 12:27:48 -0300

    C - MEX rose.cpp

--- Codeforces/Div 3 - 1054/C - MEX rose.cpp
+++ Codeforces/Div 3 - 1054/C - MEX rose.cpp
@@ -15,7 +15,17 @@ typedef long long ll;
 typedef unsigned long long ull;
 const int M = 1e9 + 7;
 
+/*
+    A solução é o maior valor entre tirar todos os iguais a K ou tirar todos os elementos menores que K.
 
+    é o max porque da pra fazer as duas coisas ao mesmo tempo, tipo se k = 4 em [1, 2, 4, 5, 5]
+
+    v. qtd para tirar k do vetor = 1 -> isso de ans1
+    u. qtd para colocar os menores que k = 2 -> faltando 0 e 3.
+    
+    mas consigo colocar 3 no lugar de 4 e 0 no lugar de algum 5, ou seja consigo colocar o menor que k e substituir k.
+    
+*/
 
 int main()
 {
